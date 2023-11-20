a3b91d16ef1619718015b5df34bf506f516a1186
İpek Baş <c2211013@student.cankaya.edu.tr>
2023-11-20 20:05:52 +0300

    cengg105.3

--- ceng105cod2.cpp
+++ ceng105cod2.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int grade, grade_2;
+    int grade, grade_2, grade_classavg = 57;
     printf("Enter your grade: ");
     scanf("%d", &grade);
     if (grade < 40) {
@@ -14,32 +14,38 @@ int main() {
     grade_2 = grade;
 
     if (grade_2 >= 90 && grade_2 <= 100) {
-        printf("Your grade is AA");
+        printf("Your grade is AA\n");
     }
     else if (grade_2 >= 85 && grade_2 <= 89) {
-        printf("Your grade is BA");
+        printf("Your grade is BA\n");
     }
     else if (grade_2 >= 80 && grade_2 <= 84) {
-        printf("Your grade is BB");
+        printf("Your grade is BB\n");
     }
     else if (grade_2 >= 70 && grade_2 <= 79) {
-        printf("Your grade is CB");
+        printf("Your grade is CB\n");
     }
     else if (grade_2 >= 60 && grade_2 <= 69) {
-        printf("Your grade is CC");
+        printf("Your grade is CC\n");
     }
     else if (grade_2 >= 50 && grade_2 <= 59) {
-        printf("Your grade is DC");
+        printf("Your grade is DC\n");
     }
     else if (grade_2 >= 45 && grade_2 <= 49) {
-        printf("Your grade is DD");
+        printf("Your grade is DD\n");
     }
     else if (grade_2 >= 35 && grade_2 <= 44) {
-        printf("Your grade is FD");
+        printf("Your grade is FD\n");
     }
     else if (grade_2 >= 0 && grade_2 <= 34) {
-        printf("Your grade is FF");
+        printf("Your grade is FF\n");
     }
 
+    if (grade_classavg <= grade_2) {
+        printf("Your grade average is higher then class (57)");
+    }
+    else if (grade_classavg > grade_2) {
+        printf("Your grade average is lower then class (57)");
+    }
     return 0;
 }
